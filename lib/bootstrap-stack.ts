8a7982692e91fcea4b8898358790a57719cf44a8
Taliesin Millhouse <taliesinwrmillhouse@gmail.com>
2021-05-28 17:18:23 +1000

    feat: removed custom resources

--- lib/bootstrap-stack.ts
+++ lib/bootstrap-stack.ts
@@ -4,8 +4,6 @@ import * as iam from '@aws-cdk/aws-iam';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as go_lambda from '@aws-cdk/aws-lambda-go';
 import * as lambda_events from '@aws-cdk/aws-lambda-event-sources';
-import * as resources from '@aws-cdk/custom-resources';
-import * as route53 from '@aws-cdk/aws-route53';
 import * as s3 from '@aws-cdk/aws-s3';
 import * as ssm from '@aws-cdk/aws-ssm';
 import * as sqs from '@aws-cdk/aws-sqs';
@@ -106,57 +104,6 @@ export class BootstrapStack extends cdk.Stack {
       parameterName: `${props.prefix}-table-arn-${props.stage}`,
       tier: ssm.ParameterTier.STANDARD,
       stringValue: table.tableArn
-    });
-
-    // Fetch hosted zone via the domain name.
-    const hostedZone = route53.HostedZone.fromLookup(this, `${props.prefix}-hosted-zone-${props.stage}`, {
-      domainName: 'millhouse.dev'
-    });
-
-    // Create a custom resource to verify the domain.
-    const verifyDomainIdentity = new resources.AwsCustomResource(this, `${props.prefix}-verify-domain-identity-${props.stage}`, {
-      onCreate: {
-        service: 'SES',
-        action: 'verifyDomainIdentity',
-        parameters: {
-          Domain: 'millhouse.dev'
-        },
-        physicalResourceId: resources.PhysicalResourceId.fromResponse('VerificationToken')
-      },
-      onDelete: {
-        service: 'SES',
-        action: 'deleteIdentity',
-        parameters: {
-          Identity: 'millhouse.dev'
-        }
-      },
-      policy: resources.AwsCustomResourcePolicy.fromStatements([
-        new iam.PolicyStatement({
-          actions: [
-            'ses:VerifyDomainIdentity'
-          ],
-          resources: [
-            '*'
-          ]
-        }),
-        new iam.PolicyStatement({
-          actions: [
-            'ses:DeleteIdentity'
-          ],
-          resources: [
-            `arn:aws:ses:${props.env.region}:${props.env.account}:identity/millhouse.dev`
-          ]
-        })
-      ])
-    });
-
-    // Create a TXT record with the SES verification token.
-    new route53.TxtRecord(this, `${props.prefix}-ses-verification-record-${props.stage}`, {
-      zone: hostedZone,
-      recordName: '_amazonses.millhouse.dev',
-      values: [
-        verifyDomainIdentity.getResponseField('VerificationToken')
-      ]
     });
   }
 }
